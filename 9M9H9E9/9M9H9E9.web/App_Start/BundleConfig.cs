bfa9cda3929b5f9ac29377a3802d2dfe3ad439e4
Dan Sansom <dan.sansom@gmail.com>
2016-04-28 15:13:13 +0100

    Updated formatting Added view for vieweing most recent posts first Removed uncessary template files

--- 9M9H9E9/9M9H9E9.web/App_Start/BundleConfig.cs
+++ 9M9H9E9/9M9H9E9.web/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace _9M9H9E9.web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            /*
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -23,9 +24,8 @@ namespace _9M9H9E9.web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            */
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
         }
     }
 }
--- 9M9H9E9/9M9H9E9.web/Controllers/HomeController.cs
+++ 9M9H9E9/9M9H9E9.web/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Web.Mvc;
 using _9M9H9E9.Data;
-
+using System.Linq;
 namespace _9M9H9E9.web.Controllers
 {
     public class HomeController : Controller
@@ -11,18 +11,10 @@ namespace _9M9H9E9.web.Controllers
             return View(postRepo.GetAll());
         }
 
-        public ActionResult About()
+        public ActionResult Recent()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            PostRepository postRepo = new PostRepository();
+            return View(from p in postRepo.GetAll() orderby p.Posted descending select p);
         }
     }
 }
